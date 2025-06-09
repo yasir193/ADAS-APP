6b2ee02cd97c1945dd9cde276bc7be374edc5835
yasir193 <ya0100922@gmail.com>
2025-06-09 16:17:06 +0300

    update in header

--- src/Components/Home/Dashboard.jsx
+++ src/Components/Home/Dashboard.jsx
@@ -117,7 +117,7 @@ export default function Dashboard() {
       
       <div className={styles.mapsCameraContainer}>
         <div className={styles.mapsBox}>
-          <h2 className="text-white">Google Maps</h2>
+          <h2 className="text-white">Live Tracker</h2>
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d27641.608998877786!2d31.1394304!3d30.002380799999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2seg!4v1739757159769!5m2!1sen!2seg"
             width="100%"
